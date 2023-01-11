161de084859256c2edebac29187c7357cd9a813a
Andrii-888 <curupa8888@gmail.com>
2023-01-11 16:57:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        char c = 'z';
+        System.out.println(c);
+        boolean b = false;
+        System.out.println(b);
+        float f = 10.256f;
+        System.out.println(f);
+        byte r = '9';
+        System.out.println(9);
+        short d = '7';
+        System.out.println(7);
+        int s = '3';
+        System.out.println(3);
+        long j = '5';
+        System.out.println(5);
+    }
+}
